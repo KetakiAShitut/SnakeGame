a093e5874e797a2281199512c1d8571a394e749b
KetakiAShitut <87990037+KetakiAShitut@users.noreply.github.com>
2021-07-26 20:49:12 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -10,7 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
-
+scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -21,8 +23,24 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
+    #to detect collision with the wall
+    if snake.head.xcor() == -280 or snake.head.xcor() == 280 or snake.head.ycor() == 280 or snake.head.ycor() == -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #to detect collision with the food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+
+    #to detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
